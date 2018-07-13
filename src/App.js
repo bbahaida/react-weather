5d25ca33ba1a09d4b07e80da208f089a29a16ce1
Brahim Bahaida <bbahieda@gmail.com>
2018-07-13 22:18:19 +0000

    Change http by https in api invokation

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class WeatherDisplay extends Component {
   }
   componentDidMount() {
     const zip = this.props.zip;
-    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" +
       zip +
       "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
     fetch(URL).then(res => res.json()).then(json => {
@@ -30,7 +30,7 @@ class WeatherDisplay extends Component {
     const weatherData = this.state.weatherData;
     if (!weatherData) return <div>Loading</div>;
     const weather = weatherData.weather[0];
-    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    const iconUrl = "https://openweathermap.org/img/w/" + weather.icon + ".png";
     return (
       <div>
         <h1>
@@ -57,36 +57,36 @@ class App extends Component {
     const activePlace = this.state.activePlace;
     return (
       <div>
-  <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        React Simple Weather App
-      </Navbar.Brand>
-    </Navbar.Header>
-  </Navbar>
-  <Grid>
-    <Row>
-      <Col md={4} sm={4}>
-        <h3>Select a city</h3>
-        <Nav
-          bsStyle="pills"
-          stacked
-          activeKey={activePlace}
-          onSelect={index => {
-            this.setState({ activePlace: index });
-          }}
-        >
-          {PLACES.map((place, index) => (
-            <NavItem key={index} eventKey={index}>{place.name}</NavItem>
-          ))}
-        </Nav>
-      </Col>
-      <Col md={8} sm={8}>
-        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
-      </Col>
-    </Row>
-  </Grid>
-</div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              React Simple Weather App
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav
+                bsStyle="pills"
+                stacked
+                activeKey={activePlace}
+                onSelect={index => {
+                  this.setState({ activePlace: index });
+                }}
+              >
+                {PLACES.map((place, index) => (
+                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
